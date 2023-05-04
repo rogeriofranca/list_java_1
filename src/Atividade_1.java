78ae4a8c4e87f66854dddcd3afaf4408e77f0ca2
rogeriofranca <rogeriofranca715@gmail.com>
2023-05-04 16:46:53 -0300

    my first activity

---
+++ src/Atividade_1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//1. Elabore um algoritmo que leia dois números e mostre a soma deles.
+public class Atividade_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOne, numberTwo, sum;
+
+        System.out.println("enter number one: ");
+        numberOne = scanner.nextInt();
+        System.out.println("enter number two: ");
+        numberTwo = scanner.nextInt();
+
+        sum = numberOne + numberTwo;
+
+        System.out.println(" the result is: " +sum);
+
+    }
+}
